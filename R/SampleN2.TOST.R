2444848ac73da6eb42d7f4139b33281ea1daf3fb
Helmut01 <helmut.schuetz@chello.at>
2017-08-24 18:46:07 +0200

    sampleN2.TOST public, man page added

--- R/SampleN2.TOST.R
+++ R/SampleN2.TOST.R
@@ -3,18 +3,19 @@
 # pooled analysis one degree of freedom less than in the common
 # model due to the 'stage'-term).
 #
-# Author: H. Schuetz
+# Author: H. Schuetz, modified by D. Labes
 # ----------------------------------------------------------------
-sampleN2.TOST <- function(alpha=0.0304, CV, n1, theta1=0.8, theta2=1.25,
-                          theta0=0.95, targetpower=0.8, design="2x2",
-                          method="exact", imax=100) 
-  { # or keep 0.0294?
-  if (missing(CV) | CV <=0) stop("CV must be given and positive.")
-  if (missing(n1) | n1 < 4) stop("n1 must be given and >=4.")
-  design <- lowcase(design)
+sampleN2.TOST <- function(alpha=0.0294, CV, n1, theta0=0.95,
+                          theta1=0.8, theta2=1.25, targetpower=0.8,
+                          design="2x2", method="exact", imax=100)
+  {
+  if (missing(CV) || CV <=0) stop("CV must be given and positive.")
+  if (missing(n1) || n1 < 4) stop("n1 must be given and >=4.")
+  if (targetpower <=0 || targetpower >= 1) stop("targetpower must be >0 and <1.")
+  design <- tolower(design)
   if (!design %in% c("2x2", "2x2x2", "parallel"))
      stop("design must be any of \"2x2\", \"2x2x2\", or \"parallel\"")
-  if(theta0<theta1 | theta0>theta2) stop("theta0 not between BE limits.")
+  if(theta0 < theta1 || theta0 > theta2) stop("theta0 not within BE limits.")
   # design constant in terms of ntot
   ifelse (design == "parallel", bk <- 4, bk <- 2)
   dfe   <- expression(n - 3) # pooled evaluation with additional 'stage' effect
@@ -24,7 +25,7 @@ sampleN2.TOST <- function(alpha=0.0304, CV, n1, theta1=0.8, theta2=1.25,
   ltheta2 <- log(theta2)
   diffm   <- log(theta0)
   se      <- CV2se(CV)
-  # start value
+  # start value by Zhang's method
   n       <- .sampleN0_3(alpha, targetpower, ltheta1, ltheta2, diffm, se, steps, bk)
   if (n < nmin) n <- nmin
   df  <- eval(dfe)
@@ -48,13 +49,12 @@ sampleN2.TOST <- function(alpha=0.0304, CV, n1, theta1=0.8, theta2=1.25,
     pow  <- .calc.power(alpha, ltheta1, ltheta2, diffm, sem=se*sqrt(bk/n), df, method)
     if (iter > imax) break
   }
-  # cant' become odd since search is done in steps of 2
-  n2 <- n + n %% 2 # if odd, round up to next even
-  ifelse (n2 - n1 > 0, n2 <- n2 - n1, n2 <- 0)
+  # total sample size to stage 2 sample size
+  ifelse (n - n1 > 0, n2 <- n - n1, n2 <- 0)
   if (n2 > 0) {
-    pow <- .calc.power(alpha, ltheta1, ltheta2, diffm, sem=se*sqrt(bk/(n1+n2)), 
+    pow <- .calc.power(alpha, ltheta1, ltheta2, diffm, sem=se*sqrt(bk/(n1+n2)),
                        df=eval(n1+n2), method)
-  } else { 
+  } else {
     # usual df if study stops in stage 1
     pow <- power.TOST(alpha=alpha, theta1=theta1, theta2=theta2,
                       theta0=theta0, CV=CV, n=n1, method=method)
@@ -64,6 +64,5 @@ sampleN2.TOST <- function(alpha=0.0304, CV, n1, theta1=0.8, theta2=1.25,
                     target=targetpower)
   names(res) <- c("Design", "alpha", "CV", "theta0", "theta1", "theta2",
                   "n1", "Sample size", "Achieved power", "Target power")
-#  if (res[["Sample size"]] == 0) res[["Sample size"]] <- NA
   return(res)
 }
